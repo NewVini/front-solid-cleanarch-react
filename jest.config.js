be112205535a53feb8501c771d08fded94e275c7
vinieu <vincius.machado8@gmail.com>
2022-06-25 22:02:32 -0300

    feat: adding all packages and tests

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}'
+  ],
+  coverageDirectory: 'coverage',
+  testEnviroment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,32 @@
+import { AxiosHttpClientAdapter } from "./axios-http-client"
+import { mockAxios } from "@/infra/test"
+import { mockPostRequest } from "@/data/test"
+import axios from 'axios'
+
+jest.mock('axios')
+
+type SutTypes = {
+    sut: AxiosHttpClientAdapter
+    mockedAxios: jest.Mocked<typeof axios>
+}
+
+const makeSut = (): SutTypes => {
+    const sut = new AxiosHttpClientAdapter()
+    const mockedAxios = mockAxios()
+    return {sut,mockedAxios}
+}
+
+describe('AxiosHttpClient', () => {
+    test('should call axios with correct values', async () => {
+        const request = mockPostRequest()
+        const {sut, mockedAxios} = makeSut()
+        await sut.post(request)
+        expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+    })
+
+    test('should call axios with correct statusCode and body', () => {
+        const {sut, mockedAxios} = makeSut()
+        const promise =  sut.post(mockPostRequest())
+        expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
+    })
+})
