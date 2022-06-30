b3f99956fdacfad105459d303781f5cba843a64d
vinieu <vincius.machado8@gmail.com>
2022-06-30 17:00:22 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
+/* eslint-disable indent */
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    mode:'development',
+    mode: 'development',
     entry: './src/main/index.tsx',
     output: {
         path: path.join(__dirname, 'public/js'),
@@ -10,21 +11,46 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
+        extensions: ['.ts', '.tsx', '.js', 'scss'],
         alias: {
             '@': path.join(__dirname, 'src')
         }
     },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                loader: 'ts-load',
+                exclude: /node_modules/
+            }, {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    },
     devServer: {
         contentBase: './public',
         writeToDisk: true,
-        historyApiFallback:true
+        historyApiFallback: true
     },
     externals: {
         react: 'React',
-        'react-dom':'ReactDOM'
+        'react-dom': 'ReactDOM'
     },
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin()
     ]
 }
