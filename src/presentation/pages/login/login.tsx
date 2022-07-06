144861ace6446c60281211744f9097c31b5f3e1b
vinieu <vincius.machado8@gmail.com>
2022-07-05 21:02:36 -0300

    chore: add login

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -3,7 +3,24 @@ import Styles from './login-styles.scss'
 
 const Login: React.FC = () => {
   return (
-    <div className={Styles.login}>hellow Clean Archithecture</div>
+    <div className={Styles.login}>
+      <header className={Styles.header}>
+        <h1>Vini - Enquetes para programadores</h1>
+      </header>
+      <form className={Styles.form}>
+        <h2>Login</h2>
+        <div className={Styles.inputWrap}>
+          <input type="email" name="email" placeholder="Digite seu e-mail" id="" />
+          <span className={Styles.status}>🔴</span>
+        </div>
+        <div className={Styles.inputWrap}>
+          <input type="password" name="password" placeholder="Digite sua senha" id="" />
+          <span className={Styles.status}>🔴</span>
+        </div>
+        <button type="submit" className={Styles.submit}>Entrar</button>
+      </form>
+      <footer className={Styles.footer}></footer>
+    </div>
   )
 }
 
